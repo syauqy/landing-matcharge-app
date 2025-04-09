25c0a287a2a3d077b84378c88ba43fa914f0cf76
Syauqy Nurul Aziz <syauqyaziz@gmail.com>
2025-04-09 23:03:37 +0700

    fix(deeplink): fixing the deep link

--- context/AppUrlListener.js
+++ context/AppUrlListener.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { App } from "@capacitor/app";
 import { useEffect } from "react";
+import Router from "next/router";
 
 export function AppUrlListener() {
+  const router = Router;
   useEffect(() => {
     App.addListener("appUrlOpen", (event) => {
       const slug = event.url.split(".app").pop();
-      if (slug) window.location.href = slug;
+      if (slug) router.push(slug);
     });
   }, []);
   return <></>;
