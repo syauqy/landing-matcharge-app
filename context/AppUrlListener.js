92791731c5098b638b051d722ed84aef7a277e31
syauqy <syauqyaziz@gmail.com>
2025-04-21 01:31:12 +0700

    fix(app): add logs to the app url listeners

--- context/AppUrlListener.js
+++ context/AppUrlListener.js
@@ -5,8 +5,19 @@ import Router from "next/router";
 
 export function AppUrlListener() {
   const router = Router;
+
   useEffect(() => {
     App.addListener("appUrlOpen", (event) => {
+      console.log("App opened with URL:", url); // Log the full URL object or string
+      console.log("URL fragment:", url?.hash || window.location.hash); // Specifically log the fragment
+      // Check if the URL string contains a '#' symbol
+      const hasHash = event.url.includes("#");
+      console.log("AppUrlListener: Does URL contain fragment (#)?", hasHash);
+      if (hasHash) {
+        // Log the part after the # if it exists
+        console.log("AppUrlListener: Fragment:", event.url.split("#")[1]);
+      }
+
       const slug = event.url.split("wetonai.vercel.app").pop();
       console.log("App URL Opened:", slug);
       if (slug) router.push(slug);
--- pages/index.js
+++ pages/index.js
@@ -84,11 +84,11 @@ export default function HomePage() {
     const isNative = Capacitor.isNativePlatform();
     let redirectUrl = "https://wetonai.vercel.app/home";
 
-    if (isNative) {
-      redirectUrl += "?native_redirect=true";
-    }
+    // if (isNative) {
+    //   redirectUrl += "?native_redirect=true";
+    // }
 
-    console.log(`Using redirectTo: ${redirectUrl}`);
+    // console.log(`Using redirectTo: ${redirectUrl}`);
 
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "apple",
