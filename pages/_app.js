7f325731de07c94738b470a68d7aec2997545b45
syauqy <syauqyaziz@gmail.com>
2025-04-21 00:45:19 +0700

    fix(home): change the authprovider

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import "@/styles/globals.css";
-import { AuthProvider } from "@/context/AuthContext";
+import { AuthProvider, useAuth } from "@/context/AuthContext";
 
 import { AppUrlListener } from "@/context/AppUrlListener";
 import Layout from "@/components/layout";
 
 export default function MyApp({ Component, pageProps }) {
+  const { user, loading: authLoading } = useAuth();
+
+  if (authLoading) {
+    return <div>Loading...</div>; // Show loading state while auth is loading
+  }
+
   return (
     <AuthProvider>
       <Layout>
